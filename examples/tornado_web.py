5ac2ea041a623d704321efb2584fd7b947396da7
mollas <mollas@users.noreply.github.com>
2018-03-29 13:34:13 +0200

    example corrections
    TypeError: 'tuple' object is not callable
    (_mysql_exceptions.OperationalError) (1071, 'Specified key was too long; max key length is 767 bytes')

--- examples/tornado_web.py
+++ examples/tornado_web.py
@@ -14,7 +14,7 @@ class User(DeclarativeBase):
     __tablename__ = 'users'
 
     id = Column(BigInteger, primary_key=True)
-    username = Column(String(255), unique=True)
+    username = Column(String(128), unique=True)
 
 
 class SyncWebRequestHandler(RequestHandler, SessionMixin):
@@ -46,9 +46,9 @@ if __name__ == '__main__':
     session_factory = make_session_factory(options.database_url)
 
     Application([
-        (r'/sync', SyncWebRequestHandler)
-        (r'/async', AsyncWebRequestHandler)
-        (r'/uses-self-session', UsesSelfSessionRequestHandler)
+        (r'/sync', SyncWebRequestHandler),
+        (r'/async', AsyncWebRequestHandler),
+        (r'/uses-self-session', UsesSelfSessionRequestHandler),
     ], session_factory=session_factory).listen(8888)
 
     IOLoop.current().start()
