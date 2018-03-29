25f8dd5ee2dd148fc64d5e70d1c14ab1006c6e70
mollas <mollas@users.noreply.github.com>
2018-03-29 14:14:15 +0200

    new function example

--- examples/tornado_web.py
+++ examples/tornado_web.py
@@ -1,11 +1,12 @@
 from sqlalchemy import Column, BigInteger, String
 from tornado.gen import coroutine
 from tornado.ioloop import IOLoop
-from tornado.options import options
-from tornado_sqlalchemy import (as_future, declarative_base,
+from tornado.options import define, options
+from tornado_sqlalchemy import (as_future, declarative_base, call_blocking,
                                 make_session_factory, SessionMixin)
 from tornado.web import RequestHandler, Application
 
+define("database-url", default="mysql+mysqldb://user:pass@localhost/db", help="DB uri")
 
 DeclarativeBase = declarative_base()
 
@@ -24,6 +25,12 @@ class SyncWebRequestHandler(RequestHandler, SessionMixin):
 
         self.write('{} users so far!'.format(count))
 
+class AnotherAsyncSyncWebRequestHandler(RequestHandler, SessionMixin):
+    async def get(self):
+        with self.make_session() as session:
+            count = await call_blocking(session.query(User).count)
+
+        self.write('{} users so far!'.format(count))
 
 class AsyncWebRequestHandler(RequestHandler, SessionMixin):
     @coroutine
@@ -43,11 +50,15 @@ class UsesSelfSessionRequestHandler(RequestHandler, SessionMixin):
 
 
 if __name__ == '__main__':
+    options.parse_command_line()
     session_factory = make_session_factory(options.database_url)
-
+    #DeclarativeBase.metadata.drop_all(session_factory.engine)
+    DeclarativeBase.metadata.create_all(session_factory.engine)
+    
     Application([
         (r'/sync', SyncWebRequestHandler),
         (r'/async', AsyncWebRequestHandler),
+        (r'/aasync',AnotherAsyncSyncWebRequestHandler),
         (r'/uses-self-session', UsesSelfSessionRequestHandler),
     ], session_factory=session_factory).listen(8888)
 
